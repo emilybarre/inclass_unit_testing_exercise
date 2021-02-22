bdc7849acd7a5aad86fd91f37c1c67624d542276
Emily Barre <emily.barre@duke.edu>
2021-02-22 17:08:34 -0500

    Created a welcome message for the program to orient the user about what the program does and what they need to be prepared to enter. This is for ease of use.

--- find_y.py
+++ find_y.py
@@ -1,3 +1,7 @@
+def interface():
+    print("Welcome! This program can be used to calculate the y value of a point on a line.")
+    print("In order to do so, the x value of that point, and the x and y values of two others, must be entered.")
+    
 def get_input():
     x1 = input("Please enter the x value of the first point on the line: ")
     y1 = input("Please enter the y value of the first point on the line: ")
