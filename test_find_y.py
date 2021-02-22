ba19275c9c854c724f8c0bc640b0ea691e2ea821
Emily Barre <emily.barre@duke.edu>
2021-02-22 18:02:27 -0500

    Added an additional unit test for evaluating the convert_input() function that was developed. This is to ensure that the conversion is working as expected.

--- test_find_y.py
+++ test_find_y.py
@@ -26,3 +26,16 @@ def test_calc_y_int():
     expected_result = 0
     result = calc_y_int(x1,y1,m)
     assert result == expected_result
+
+def test_convert_input(x1, y1, x2, y2):
+    from find_y import convert_input
+    x1 = "1"
+    y1 = "1"
+    x2 = "2"
+    y2 = "2"
+    expected_result1 = (1, 1)
+    expected_result2 = (2, 2)
+    (result1, result2) = convert_input(x1, y1, x2, y2)
+    assert result1 == expected_result1
+    assert result2 == expected_result2
+    
