c69868f1eaafd4ea5c3af00c82b838c7985a3d9f
Emily Barre <emily.barre@duke.edu>
2021-02-22 17:11:57 -0500

    Added a driver function that will be used to call the underlying functions in the program. It currently only calls the interface and get_input functions, but is designed to host all of the other modular functions necessary for a robust system in the future.

--- find_y.py
+++ find_y.py
@@ -7,4 +7,8 @@ def get_input():
     y1 = input("Please enter the y value of the first point on the line: ")
     x2 = input("Please enter the x value of the second point on the line: ")
     y2 = input("Please enter the y value of the second point on the line: ")
-    return x1, y2, x2, y2
+    return x1, y1, x2, y2
+
+def driver():
+    interface()
+    (x1, y1, x2, y2) = get_input()
