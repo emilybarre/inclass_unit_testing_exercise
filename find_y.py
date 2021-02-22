d2be4bbb87f1ba77789c49bfd54409dbcac1010a
Emily Barre <emily.barre@duke.edu>
2021-02-22 17:45:05 -0500

    Made minor cosmetic changes to the code in order to be pycodestyle compliant. This was necessary because incorporating continuous integration means that these cosmetic changes must be up to date in order to merge with the main branch.

--- find_y.py
+++ find_y.py
@@ -1,7 +1,11 @@
 def interface():
-    print("Welcome! This program can be used to calculate the y value of a point on a line.")
-    print("In order to do so, the x value of that point, and the x and y values of two others, must be entered.")
-    
+    print("Welcome!")
+    print("This can be used to calculate the y value of a point on a line.")
+    print("You will need to enter:")
+    print("- the x value of that point")
+    print("- x and y values of two other points")
+
+
 def get_input():
     x1 = input("Please enter the x value of the first point on the line: ")
     y1 = input("Please enter the y value of the first point on the line: ")
@@ -15,14 +19,14 @@ def calc_m(x1, y1, x2, y2):
     return m
 
 
-def calc_y_int(x1,y1,m):
+def calc_y_int(x1, y1, m):
     b = y1 - (m*x1)
     return b
 
 
-def calc_y(point_1,point_2,x_of_point_3):
+def calc_y(point_1, point_2, x_of_point_3):
     m = calc_m(point_1[0], point_1[1], point_2[0], point_2[1])
-    b = calc_y_int(point_1[0], point_1[1],m)
+    b = calc_y_int(point_1[0], point_1[1], m)
     y = (m*x_of_point_3) + b
     return y
 
@@ -30,6 +34,7 @@ def calc_y(point_1,point_2,x_of_point_3):
 def driver():
     interface()
     (x1, y1, x2, y2) = get_input()
+
 
 if __name__ == "__main__":
     driver()
--- test_find_y.py
+++ test_find_y.py
@@ -1,18 +1,19 @@
 def test_calc_y():
     from find_y import calc_y
-    point_1 = (1,1)
-    point_2 = (2,2)
+    point_1 = (1, 1)
+    point_2 = (2, 2)
     x_of_point_3 = 0
     expected_result = 0
     result = calc_y(point_1, point_2, x_of_point_3)
     assert result == expected_result
+
 
 def test_calc_m():
     from find_y import calc_m
     x1 = 1
     y1 = 1
     x2 = 2
-    y2 = 2 
+    y2 = 2
     expected_result = 1
     result = calc_m(x1, y1, x2, y2)
     assert result == expected_result
@@ -24,5 +25,5 @@ def test_calc_y_int():
     y1 = 1
     m = 1
     expected_result = 0
-    result = calc_y_int(x1,y1,m)
+    result = calc_y_int(x1, y1, m)
     assert result == expected_result
