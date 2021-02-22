11f4a3be07dff3e5a5c0601f7640fd4179071070
Emily Barre <emily.barre@duke.edu>
2021-02-22 16:52:48 -0500

    Wrote a unit testing function for the slope calculation. Calculating the slope will be a part of calculating the equation for the line in order to find the y value of an x that has been asked for. Because this slope calculation is an independent entity, it should be tested independently for clarity, and therefore this function was written.

--- test_find_y.py
+++ test_find_y.py
@@ -1,4 +1,4 @@
-def test_find_y():
+def test_calc_y():
     from find_y import calc_y
     x1 = 1
     y1 = 1
@@ -6,7 +6,17 @@ def test_find_y():
     y2 = 2
     x = 0
     expected_result = 0
-
     result = calc_y(x1, y1, x2, y2, x)
-
     assert result == expected_result
+
+def test_calc_m():
+    from find_y import calc_m
+    x1 = 1
+    y1 = 1
+    x2 = 2
+    y2 = 2 
+    expected_result = 1
+    result = calc_y(x1, y1, x2, y2, x)
+    assert result == expected_result
+
+
