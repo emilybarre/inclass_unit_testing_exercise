9c545d7051f0a14382d910e4c480da678d2696bd
Emily Barre <emily.barre@duke.edu>
2021-02-22 17:05:11 -0500

    Created a function called get_input that allows the user to input the x and y values of the two points on the line

---
+++ find_y.py
@@ -0,0 +1,6 @@
+def get_input():
+    x1 = input("Please enter the x value of the first point on the line: ")
+    y1 = input("Please enter the y value of the first point on the line: ")
+    x2 = input("Please enter the x value of the second point on the line: ")
+    y2 = input("Please enter the y value of the second point on the line: ")
+    return x1, y2, x2, y2
